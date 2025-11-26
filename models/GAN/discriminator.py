a533fded3fc1e9a41fa99b88ab3c9afa025e7f4e
drawcodeboy <20rnjsekdns@naver.com>
2025-11-27 00:39:51 +0900

    [Update] simplify Generator & Discriminator arameters

--- models/GAN/discriminator.py
+++ models/GAN/discriminator.py
@@ -6,10 +6,8 @@ class Discriminator(nn.Module):
                  img_dim=(1, 28, 28)):
         super().__init__()
         self.img_dim = img_dim
-        self.li1 = nn.Linear(self.img_dim[0]*self.img_dim[1]*self.img_dim[2], 1024)
-        self.li2 = nn.Linear(1024, 512)
-        self.li3 = nn.Linear(512, 256)
-        self.li4 = nn.Linear(256, 1)
+        self.li1 = nn.Linear(self.img_dim[0]*self.img_dim[1]*self.img_dim[2], 256)
+        self.li2 = nn.Linear(256, 1)
 
         self.acti = nn.LeakyReLU()
         self.sigmoid = nn.Sigmoid()
@@ -17,7 +15,5 @@ class Discriminator(nn.Module):
     def forward(self, x):
         x = x.view(-1, self.img_dim[0]*self.img_dim[1]*self.img_dim[2])
         x = self.acti(self.li1(x))
-        x = self.acti(self.li2(x))
-        x = self.acti(self.li3(x))
-        y = self.sigmoid(self.li4(x))
+        y = self.sigmoid(self.li2(x))
         return y
--- models/GAN/generator.py
+++ models/GAN/generator.py
@@ -9,16 +9,12 @@ class Generator(nn.Module):
         super().__init__()
         self.img_dim = img_dim
         self.li1 = nn.Linear(100, 256)
-        self.li2 = nn.Linear(256, 512)
-        self.li3 = nn.Linear(512, 1024)
-        self.li4 = nn.Linear(1024, self.img_dim[0]*self.img_dim[1]*self.img_dim[2])
+        self.li2 = nn.Linear(256, self.img_dim[0]*self.img_dim[1]*self.img_dim[2])
 
         self.acti = nn.ReLU()
 
     def forward(self, z):
         z = self.acti(self.li1(z))
-        z = self.acti(self.li2(z))
-        z = self.acti(self.li3(z))
-        x = F.sigmoid(self.li4(z))
+        x = F.sigmoid(self.li2(z))
         x = x.view(-1, self.img_dim[0], self.img_dim[1], self.img_dim[2])
         return x
