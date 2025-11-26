3f1011319eeeebc5e199d9827c689a0582fcd674
drawcodeboy <20rnjsekdns@naver.com>
2025-11-27 00:22:23 +0900

    [Update] use model.generate()

--- subtasks/01_generate/exec.py
+++ subtasks/01_generate/exec.py
@@ -15,20 +15,20 @@ def main():
         'img_dim': (1, 28, 28),
         'z_dim': 100}).to(device)
 
-    ckpt = torch.load("saved/weights/gan.mnist.epochs_060.pth", 
+    ckpt = torch.load("saved/weights/gan.mnist.epochs_100.pth", 
                       map_location=device,
                       weights_only=False)
-    z = torch.randn(1, model.z_dim).to(device)
+    model.load_state_dict(ckpt['model'])
 
-    model.eval()
-    with torch.no_grad():
-        x_prime = model.generator(z)
-    x_prime = rearrange(x_prime, '1 1 h w -> h w 1')
-    x_prime = x_prime.cpu().numpy() * 255.0
-    print(x_prime.shape, x_prime.dtype)
-    x_prime = x_prime.astype(np.uint8)
+    # don't need to call torch.no_grad(), and model.eval()
+    # model.generate() includes these functions.
+    x_prime = model.generate(n_samples=3, device=device)
 
-    cv2.imwrite("subtasks/generate/sample.png", x_prime)
+    for idx in range(1, x_prime.size(0)+1):
+        generated_img = (x_prime[0].cpu().numpy() * 255.0).astype(np.uint8)
+        generated_img = np.transpose(generated_img, (1, 2, 0)) # (C, H, W) -> (H, W, C)
+
+        cv2.imwrite(f"subtasks/01_generate/sample_{idx:02d}.png", generated_img)
 
 if __name__ == '__main__':
     main()
