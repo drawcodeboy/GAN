6a4e0e12aeabcd0c4bf39c4ff988125a340eb97e
drawcodeboy <20rnjsekdns@naver.com>
2025-11-28 23:32:11 +0900

    [Add] implement like reference repo

--- models/GAN/discriminator.py
+++ models/GAN/discriminator.py
@@ -6,14 +6,17 @@ class Discriminator(nn.Module):
                  img_dim=(1, 28, 28)):
         super().__init__()
         self.img_dim = img_dim
-        self.li1 = nn.Linear(self.img_dim[0]*self.img_dim[1]*self.img_dim[2], 256)
-        self.li2 = nn.Linear(256, 1)
+        self.li1 = nn.Linear(self.img_dim[0]*self.img_dim[1]*self.img_dim[2], 512)
+        self.li2 = nn.Linear(512, 256)
+        self.li3 = nn.Linear(256, 1)
 
-        self.acti = nn.LeakyReLU()
+        self.acti = nn.LeakyReLU(0.2, inplace=True)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = x.view(-1, self.img_dim[0]*self.img_dim[1]*self.img_dim[2])
         x = self.acti(self.li1(x))
-        y = self.sigmoid(self.li2(x))
+        x = self.acti(self.li2(x))
+        x = self.li3(x)
+        y = self.sigmoid(x)
         return y
--- models/GAN/generator.py
+++ models/GAN/generator.py
@@ -8,13 +8,28 @@ class Generator(nn.Module):
                  img_dim=(1, 28, 28)):
         super().__init__()
         self.img_dim = img_dim
-        self.li1 = nn.Linear(100, 256)
-        self.li2 = nn.Linear(256, self.img_dim[0]*self.img_dim[1]*self.img_dim[2])
+        self.li1 = nn.Linear(100, 128)
+        self.bn1 = nn.BatchNorm1d(128, 0.8)
+        
+        self.li2 = nn.Linear(128, 256)
+        self.bn2 = nn.BatchNorm1d(256, 0.8)
 
-        self.acti = nn.ReLU()
+        self.li3 = nn.Linear(256, 512)
+        self.bn3 = nn.BatchNorm1d(512, 0.8)
+
+        self.li4 = nn.Linear(512, 1024)
+        self.bn4 = nn.BatchNorm1d(1024, 0.8)
+
+        self.li5 = nn.Linear(1024, self.img_dim[0]*self.img_dim[1]*self.img_dim[2])
+
+        self.acti = nn.LeakyReLU(0.2, inplace=True)
 
     def forward(self, z):
-        z = self.acti(self.li1(z))
-        x = F.sigmoid(self.li2(z))
+        z = self.acti(self.bn1(self.li1(z)))
+        z = self.acti(self.bn2(self.li2(z)))
+        z = self.acti(self.bn3(self.li3(z)))
+        z = self.acti(self.bn4(self.li4(z)))
+        z = self.li5(z)
+        x = F.sigmoid(z) # different with referece repository, they used tanh().
         x = x.view(-1, self.img_dim[0], self.img_dim[1], self.img_dim[2])
         return x
