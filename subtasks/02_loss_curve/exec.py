25b0ed8fea7899107b4b7b56c1acfeb6355bea03
drawcodeboy <20rnjsekdns@naver.com>
2025-11-28 23:49:58 +0900

    [Add] ylim

--- subtasks/02_loss_curve/exec.py
+++ subtasks/02_loss_curve/exec.py
@@ -1,22 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--optim', '-o', default='sgd')
+
+    args = parser.parse_args()
+    optim = args.optim
+
     plt.figure(figsize=(8, 4))
 
-    optim = 'sgd'
-    
-    d_loss = np.load("saved/loss/train_loss_discriminator_gan.mnist.{optim}.npy")
-    g_loss = np.load("saved/loss/train_loss_generator_gan.mnist.{optim}.npy")
+    d_loss = np.load(f"saved/loss/train_loss_discriminator_gan.mnist.{optim}.npy")
+    g_loss = np.load(f"saved/loss/train_loss_generator_gan.mnist.{optim}.npy")
 
     epochs = np.array([i + 1 for i in range(0, len(d_loss))])
 
     plt.plot(epochs, d_loss, label='Discriminator', color='blue')
     plt.plot(epochs, g_loss, label='Generator', color='red')
 
-    plt.title("Loss Curve")
+    plt.title(f"Loss Curve (optim: {optim})")
     plt.xlabel("Epochs")
     plt.ylabel("Minimax Loss")
+
+    plt.ylim(-0.2, 7)
 
     plt.legend()
     plt.tight_layout()
