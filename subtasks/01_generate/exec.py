58bc878bd2fece226091e8d5fc38f7ba23d5a1d5
drawcodeboy <20rnjsekdns@naver.com>
2025-11-29 00:30:57 +0900

    [Complete] All experiments

--- subtasks/01_generate/exec.py
+++ subtasks/01_generate/exec.py
@@ -5,6 +5,7 @@ sys.path.append(os.getcwd())
 from models import load_model
 import cv2
 import numpy as np
+import argparse
 
 def main():
     device = 'cuda:0'
@@ -13,20 +14,26 @@ def main():
         'img_dim': (1, 28, 28),
         'z_dim': 100}).to(device)
 
-    ckpt = torch.load("saved/weights/gan.mnist.epochs_100.pth", 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--optim', '-o', default='sgd')
+    
+    args = parser.parse_args()
+    optim = args.optim
+
+    ckpt = torch.load(f"saved/weights/gan.mnist.{optim}.epochs_200.pth", 
                       map_location=device,
                       weights_only=False)
     model.load_state_dict(ckpt['model'])
 
     # don't need to call torch.no_grad(), and model.eval()
     # model.generate() includes these functions.
-    x_prime = model.generate(n_samples=2, device=device)
+    x_prime = model.generate(n_samples=3, device=device)
 
     for idx in range(1, x_prime.size(0)+1):
         generated_img = (x_prime[idx-1].cpu().numpy() * 255.0).astype(np.uint8)
         generated_img = np.transpose(generated_img, (1, 2, 0)) # (C, H, W) -> (H, W, C)
 
-        cv2.imwrite(f"assets/generation_results_mnist/sample_{idx:02d}.jpg", generated_img)
+        cv2.imwrite(f"assets/generation_results_mnist/sample_{idx:02d}_{optim}.jpg", generated_img)
 
 if __name__ == '__main__':
     main()
